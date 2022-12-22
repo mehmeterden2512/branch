011d50778128e493d4856a12f85b6d3281fe9e0b
Mehmet Erden <merden2512@gmail.com>
2022-12-22 13:54:31 +0300

    braanch versiyon2

---
+++ src/B2.java
@@ -0,0 +1,23 @@
+public class B2 {
+    public static void main(String[] args) {
+        /*
+        temelde git ve gitHub icin 3 asamadan bahsedebiliriz
+        1-projemiz/kasorumuzu git ile taniştırma
+                git init
+
+                2-projemiz/klasorumuz icin versiyon olusturma
+                    git add .
+                    git commit -m "vesiyonismi"
+
+                    3-gitHub ile entegrasyon ve senkronizasyon
+                    -ilk defe entegre etmek icin once gitHub'da yeni repo olusturup
+                    oradaki son 3 adimi kopyalayıp terminal/CMD ye yapiştirip
+                    ENTER'a basacagiz
+                    - ilk entegrasyondan sonra
+                    her degisşiklik yaptıgımızda
+                    -git add .
+                    -git commit - m" versiyon adi"
+                    -git push
+         */
+    }
+}
